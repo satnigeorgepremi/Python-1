6e9e0c83f85e4a0075a0a596ca46224e8320acaf
VimalDhas <41019951+VimalDhas@users.noreply.github.com>
2018-07-17 21:33:54 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+a=int(input("first no"))
+b=int(input("second no"))
+c=int(input("third no"))
+if a>b and a>c:
+    print(" a is big")
+elif b>c:
+    print(" b is big")
+else:
+    print("c is big")
