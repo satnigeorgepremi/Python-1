0e7bd12010ee3a159c4a9f41fee065ffeef37ae8
VimalDhas <41019951+VimalDhas@users.noreply.github.com>
2018-09-06 10:39:20 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,12 @@
+a=int(input("enter the no"))
+if a > 1:
+
+    for i in range(2, a):
+        if (a % i) == 0:
+            print(a, "is not a prime number")
+            print(i, "times", a// i, "is", a)
+            break
+    else:
+        print(a, "yes")
+else:
+    print(a, "is not a prime number")
