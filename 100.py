572c6cc9dbac7634cc95b817641a14c9e4399fe0
VimalDhas <41019951+VimalDhas@users.noreply.github.com>
2018-09-21 22:42:29 +0530

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,8 +1,9 @@
 a=input("enter the number")
-b=len(a)
-i=0
-f=1
-while i<int(b):
-    f=f*int(a[i])
-    i+=1
-print(f)
+if(a<=100000000000):
+    b=len(a)
+    i=0
+    f=1
+    while i<int(b):
+        f=f*int(a[i])
+        i+=1
+    print(f)
